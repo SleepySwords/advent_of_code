f591b214090b1cad71c1f88095cc1b39564f956f
Swords <33922797+SleepySwords@users.noreply.github.com>
2021-12-02 23:03:23 +1100

    FEAT: basic stuff + Days 1 + 2

---
+++ README.md
@@ -0,0 +1,8 @@
+# advent_of_code
+Each year is associated with a branch
+
+The events in this repo:
+- [2021](../../tree/2021)
+
+This branch uses the boilerplate file structure in addition to AOC_SESSION enviorment variable.
+(I should clean this boilercode up at some point)
---
+++ boilerplate/src/solution.rs
@@ -0,0 +1,15 @@
+
+use utils;
+
+pub struct Day;
+
+impl utils::Solution for Day {
+    fn part1(&self, input: &str) -> String {
+        String::from("Not implemented!")
+    }
+
+    fn part2(&self, input: &str) -> String {
+        String::from("Not implemented!")
+    }
+}
+

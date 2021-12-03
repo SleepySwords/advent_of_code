0e18f693a77ad775afb6625dddf1e832f46ee1fd
Swords <33922797+SleepySwords@users.noreply.github.com>
2021-12-03 22:06:58 +1100

    REFACTOR: refactor day3 to remove clones and formatting

--- day3/src/solution.rs
+++ day3/src/solution.rs
@@ -24,38 +24,47 @@ impl utils::Solution for Day {
     }
 
     fn part2(&self, input: &str) -> String {
-        let mut data: Vec<&str> = input.split('\n').collect();
-        let binary_length = data[0].len();
+        let mut oxygen_data: Vec<&str> = input.split('\n').collect();
+        let mut co2_data: Vec<&str> = input.split('\n').collect();
+        let binary_length = oxygen_data[0].len();
 
         for index in 0..binary_length {
-            let filtered = data.clone().into_iter().filter(|&x| x.chars().nth(index).unwrap() == '1');
-            let bit_number = filtered.clone().count();
-            if bit_number >= (data.len() - bit_number) {
-                data = filtered.collect();
+            let data_with_1_bit: Vec<&str> = oxygen_data
+                .clone()
+                .into_iter()
+                .filter(|&x| x.chars().nth(index).unwrap() == '1')
+                .collect();
+
+            let size_of_1s = data_with_1_bit.len();
+            if size_of_1s >= (oxygen_data.len() - size_of_1s) {
+                oxygen_data = data_with_1_bit;
             } else {
-                for bad_value in filtered {
-                    data.retain(|&x| x != bad_value);
+                for bad_value in data_with_1_bit {
+                    oxygen_data.retain(|&x| x != bad_value);
                 }
             }
         }
 
-        let oxygen_rating = u32::from_str_radix(data[0], 2).unwrap();
-        
-        let mut data: Vec<&str> = input.split('\n').collect();
         for index in 0..binary_length {
-            if data.len() == 1 { continue; }
-            let filtered = data.clone().into_iter().filter(|&x| x.chars().nth(index).unwrap() == '1');
-            let bit_number = filtered.count();
-            if bit_number >= (data.len() - bit_number) {
-                for bad_value in filtered {
-                    data.retain(|&x| x != bad_value);
+            if co2_data.len() == 1 { continue; }
+            let data_with_1_bit: Vec<&str> = co2_data
+                .clone()
+                .into_iter()
+                .filter(|&x| x.chars().nth(index).unwrap() == '1')
+                .collect();
+
+            let size_of_1s = data_with_1_bit.len();
+            if size_of_1s >= (co2_data.len() - size_of_1s) {
+                for bad_value in data_with_1_bit {
+                    co2_data.retain(|&x| x != bad_value);
                 }
             } else {
-                data = filtered.clone().collect();
+                co2_data = data_with_1_bit;
             }
         }
 
-        let co2_rating = u32::from_str_radix(data[0], 2).unwrap();
+        let oxygen_rating = u32::from_str_radix(oxygen_data[0], 2).unwrap();
+        let co2_rating = u32::from_str_radix(co2_data[0], 2).unwrap();
 
         (oxygen_rating * co2_rating).to_string()
     }

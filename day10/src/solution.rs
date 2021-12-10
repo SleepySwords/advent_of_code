eb4dd96659e0280a655f271045ec9c3588be64a5
Swords <33922797+SleepySwords@users.noreply.github.com>
2021-12-10 19:31:22 +1100

    REFACTOR

--- day10/src/solution.rs
+++ day10/src/solution.rs
@@ -1,21 +1,32 @@
-use std::str::Chars;
 use std::iter::Peekable;
+use std::str::Chars;
 
 use utils;
 
 pub struct Day;
 
 impl Day {
+    fn get_closing(opening: char) -> char {
+        match opening {
+            '{' => '}',
+            '(' => ')',
+            '[' => ']',
+            '<' => '>',
+            _ => '.',
+        }
+    }
+
     fn corrupt(chars: &mut Peekable<Chars>) -> Option<char> {
         let openings = vec!['{', '(', '[', '<'];
         let opening = chars.next().unwrap();
+
         let mut next = chars.peek();
         if let None = next {
             return None;
         }
         while openings.contains(next.unwrap()) {
-            let char = Self::corrupt(chars);
-            if let Some(t) = char {
+            let corrupted = Self::corrupt(chars);
+            if let Some(t) = corrupted {
                 return Some(t);
             }
             next = chars.peek();
@@ -26,36 +37,27 @@ impl Day {
         let closing = chars.next();
         return match closing {
             Some(close) => {
-                if !((opening == '[' && close == ']' ) || (opening == '(' && close == ')') || (opening == '{' && close == '}') || (opening == '<' && close == '>')) {
+                if Self::get_closing(opening) != close {
                     Some(close)
                 } else {
                     None
                 }
-            },
+            }
             None => None,
-        }
-    }
-
-    fn get_closing(opening: char) -> char {
-        match opening {
-            '{' => '}',
-            '(' => ')',
-            '[' => ']',
-            '<' => '>',
-            _ => '.'
-        }
+        };
     }
 
     fn auto_correct(chars: &mut Peekable<Chars>) -> Option<String> {
         let openings = vec!['{', '(', '[', '<'];
         let opening = chars.next().unwrap();
+
         let mut next = chars.peek();
         if let None = next {
             return Some(Self::get_closing(opening).to_string());
         }
         while openings.contains(next.unwrap()) {
-            let char = Self::auto_correct(chars);
-            if let Some(t) = char {
+            let auto = Self::auto_correct(chars);
+            if let Some(t) = auto {
                 let mut str = Self::get_closing(opening).to_string();
                 str.insert_str(0, &t);
                 return Some(str);
@@ -68,57 +70,65 @@ impl Day {
         let closing = chars.next();
         return match closing {
             Some(close) => {
-                if !((opening == '[' && close == ']' ) || (opening == '(' && close == ')') || (opening == '{' && close == '}') || (opening == '<' && close == '>')) {
+                if Self::get_closing(opening) != close {
                     panic!("They don't match")
                 } else {
                     None
                 }
-            },
+            }
             None => Some(Self::get_closing(opening).to_string()),
-        }
+        };
     }
 }
 
 impl utils::Solution for Day {
     fn part1(&self, input: &str) -> String {
-        input.split('\n').map(|x| {
-            if let Some(char) = Self::corrupt(&mut x.chars().peekable()) {
-                match char {
-                    ')' => 3,
-                    ']' => 57,
-                    '}' => 1197,
-                    '>' => 25137,
-                    _ => 0
+        input
+            .split('\n')
+            .map(|x| {
+                if let Some(char) = Self::corrupt(&mut x.chars().peekable()) {
+                    match char {
+                        ')' => 3,
+                        ']' => 57,
+                        '}' => 1197,
+                        '>' => 25137,
+                        _ => 0,
+                    }
+                } else {
+                    0
                 }
-            } else {
-                0
-            }
-        }).sum::<u32>().to_string()
-        // println!("{:?}", Self::next_char(&mut input.chars().peekable()));
-        // String::from("Not implemented!")
+            })
+            .sum::<u32>()
+            .to_string()
     }
 
     fn part2(&self, input: &str) -> String {
-        let mut data: Vec<u64> = input.split('\n').map(|x| {
-            if let None = Self::corrupt(&mut x.chars().peekable()) {
-                let mut total = 0;
-                for c in Self::auto_correct(&mut x.chars().peekable()).unwrap().chars() {
-                    total *= 5;
-                    total += match c {
-                        ')' => 1,
-                        ']' => 2,
-                        '}' => 3,
-                        '>' => 4,
-                        _ => 0
+        let mut data: Vec<u64> = input
+            .split('\n')
+            .map(|x| {
+                if let None = Self::corrupt(&mut x.chars().peekable()) {
+                    let mut total = 0;
+                    for c in Self::auto_correct(&mut x.chars().peekable())
+                        .unwrap()
+                        .chars()
+                    {
+                        total *= 5;
+                        total += match c {
+                            ')' => 1,
+                            ']' => 2,
+                            '}' => 3,
+                            '>' => 4,
+                            _ => 0,
+                        }
                     }
+                    total
+                } else {
+                    0
                 }
-                total
-            } else {
-                0
-            }
-        }).filter(|&x| x != 0).collect();
+            })
+            .filter(|&x| x != 0)
+            .collect();
         data.sort();
         data[data.len() / 2].to_string()
     }
 }
-

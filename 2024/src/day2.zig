5481baf134d7e329383a4799a237949ef6b94730
Ibby <33922797+SleepySwords@users.noreply.github.com>
2024-12-04 01:50:07 +1100

    feat: day 02 p1 in Zig

---
+++ 2024/src/day2.zig
@@ -0,0 +1,87 @@
+const std = @import("std");
+
+const ArrayListU32 = std.ArrayList(u32);
+var GPA = std.heap.GeneralPurposeAllocator(.{}){};
+
+pub fn main() !void {
+    const allocator = GPA.allocator();
+    defer if (GPA.deinit() == std.heap.Check.leak) {
+        std.debug.print("wow", .{});
+    };
+    const list = try parse(allocator);
+    defer {
+        for (list.items) |i| {
+            i.deinit();
+        }
+        list.deinit();
+    }
+
+    const ans_p1 = part1(list);
+    std.debug.print("Part 1: {d}\n", .{ans_p1});
+
+    const ans_p2 = part2(list);
+    std.debug.print("Part 2: {d}\n", .{ans_p2});
+}
+
+pub fn parse(allocator: std.mem.Allocator) !std.ArrayList(ArrayListU32) {
+    const stdin_file = std.io.getStdIn();
+    var br = std.io.bufferedReader(stdin_file.reader());
+    const stdin = br.reader();
+
+    var buf: [1000]u8 = undefined;
+    var list = std.ArrayList(ArrayListU32).init(allocator);
+    while (try stdin.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        var u32_list = ArrayListU32.init(allocator);
+        var iterator = std.mem.splitAny(u8, line, " ");
+        while (iterator.next()) |o| {
+            try u32_list.append(try std.fmt.parseInt(u32, o, 10));
+        }
+
+        try list.append(u32_list);
+    }
+
+    return list;
+}
+
+pub fn part1(list: std.ArrayList(ArrayListU32)) usize {
+    var total: usize = 0;
+    for (list.items) |line| {
+        const increasing = line.items[0] < line.items[1];
+        var window_iterator = std.mem.window(u32, line.items, 2, 1);
+        var is_valid = true;
+
+        while (window_iterator.next()) |window| {
+            const window_increasing = window[0] < window[1];
+            const distance = if (window_increasing) window[1] - window[0] else window[0] - window[1];
+
+            if (window_increasing != increasing or 1 > distance or distance > 3) {
+                is_valid = false;
+                break;
+            }
+        }
+
+        if (is_valid) total += 1;
+    }
+    return total;
+}
+
+pub fn part2(list: std.ArrayList(ArrayListU32)) usize {
+    var total: usize = 0;
+    for (list.items) |line| {
+        const increasing = line.items[0] < line.items[1];
+        var window_iterator = std.mem.window(u32, line.items, 2, 1);
+        var last_valid: usize = line.items[0];
+
+        while (window_iterator.next()) |window| {
+            const window_increasing = window[0] < window[1];
+            const distance = if (window_increasing) window[1] - window[0] else window[0] - window[1];
+
+            if (window_increasing != increasing or 1 > distance or distance > 3) {} else {
+                last_valid = window[1];
+            }
+        }
+
+        if (last_valid <= 1) total += 1;
+    }
+    return total;
+}
